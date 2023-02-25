a2203d28a659cca0bdc9c082ec717385fd2a7504
ADX <86833171+simplyadx@users.noreply.github.com>
2023-02-25 22:50:47 +0530

    init

--- assets/js/devices.js
+++ assets/js/devices.js
@@ -20,26 +20,26 @@ TweenMax.from(
     1
 );
 //
-document.getElementById("open").addEventListener("click", function () {
-    TweenMax.to(
-        ".popup-container",
-        0.25,
-        {
-            scale: 1,
-            ease: Power4.easeInOut,
-        },
-        1
-    );
-});
+// document.getElementById("open").addEventListener("click", function () {
+//     TweenMax.to(
+//         ".popup-container",
+//         0.25,
+//         {
+//             scale: 1,
+//             ease: Power4.easeInOut,
+//         },
+//         1
+//     );
+// });
 
-document.querySelector("#close").addEventListener("click", function () {
-    TweenMax.to(
-        ".popup-container",
-        0.2,
-        {
-            scale: 0,
-            ease: Power4.easeInOut,
-        },
-        1
-    );
-});
+// document.querySelector("#close").addEventListener("click", function () {
+//     TweenMax.to(
+//         ".popup-container",
+//         0.2,
+//         {
+//             scale: 0,
+//             ease: Power4.easeInOut,
+//         },
+//         1
+//     );
+// });
